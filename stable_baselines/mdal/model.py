49ec28d346808e4181694c47b83614b20a5a8e86
buinm <b.minh96@gmail.com>
2022-12-02 14:40:11 -0800

    Added FW method

--- stable_baselines/mdal/model.py
+++ stable_baselines/mdal/model.py
@@ -15,6 +15,8 @@ from gym import spaces
 from stable_baselines.common.base_class import BaseRLModel
 from stable_baselines.common.vec_env import VecEnv, VecFrameStack
 from stable_baselines.common.base_class import _UnvecWrapper
+from numpy import linalg as LA
+
 
 
 
@@ -802,6 +804,344 @@ class MWAL(object):
 
     def save(self, save_path):
         pass
+
+
+class ProjFWMethod(object):
+    def __init__(self, policy, env, expert_dataset=None,
+                 hidden_size_adversary=100, adversary_entcoeff=0, timesteps_per_batch=2000,
+                 g_step=1, d_step=10, d_stepsize=3e-4, verbose=0,
+                 _init_setup_model=True, exploration_bonus=False, bonus_coef=0.01, is_action_features=True,
+                  neural=False, lipschitz=1.0, mdpSolver=None, **kwargs):
+        self.kwargs = {
+            'policy': policy,
+            'env': env,
+            'expert_dataset': expert_dataset,
+            'hidden_size_adversary': hidden_size_adversary,
+            'adversary_entcoeff': adversary_entcoeff,
+            'timesteps_per_batch': timesteps_per_batch,
+            'g_step': g_step,
+            'd_step': d_step,
+            'd_stepsize': d_stepsize,
+            'verbose': verbose,
+            '_init_setup_model': _init_setup_model,
+            'exploration_bonus': exploration_bonus,
+            'bonus_coef': bonus_coef,
+            'is_action_features': is_action_features,
+            'neural': neural,
+            'lipschitz': lipschitz,
+        }
+        self.kwargs.update(kwargs)
+        self.mdpSolver = mdpSolver
+        self.expert_dataset = expert_dataset
+        # self.normalize_s = np.array([1./2., 1./2., 1./16., 1./4.], dtype=np.float32)
+        assert not is_action_features
+        if env.envs[0].spec.id == 'Pendulum-v0':
+            self.normalize_s = np.array([1./2., 1./2., 1./16.], dtype=np.float32)
+            self.normalize_b = np.array([0.5, 0.5, 0.5], dtype=np.float32)
+        elif env.envs[0].spec.id == 'MountainCarContinuous-v0':
+            self.normalize_s = np.array([1/1.8, 1/0.14], dtype=np.float32)
+            self.normalize_b = np.array([2./3., 0.5], dtype=np.float32)
+        expert_feat_exp = self.expert_dataset.successor_features * 100 # gamma has to be 0.99. successor_features has extra * (1 - gamma)
+        self.expert_feat_exp = expert_feat_exp[:-1] * self.normalize_s + self.normalize_b
+        self.model = None
+        self.alpha = None
+        self.logdir = kwargs['tensorboard_log']
+        if not os.path.exists(kwargs['tensorboard_log']):
+            os.makedirs(kwargs['tensorboard_log'])
+
+    def get_feature_expectation_raw(self, model, env, n_episodes=10):
+        # Retrieve the environment using the RL model
+        if env is None and isinstance(model, BaseRLModel):
+            env = model.get_env()
+
+        assert env is not None, "You must set the env in the model or pass it to the function."
+
+        is_vec_env = False
+        if isinstance(env, VecEnv) and not isinstance(env, _UnvecWrapper):
+            is_vec_env = True
+            if env.num_envs > 1:
+                warnings.warn("You are using multiple envs, only the data from the first one will be recorded.")
+
+        # Sanity check
+        assert (isinstance(env.observation_space, spaces.Box) or
+                isinstance(env.observation_space, spaces.Discrete)), "Observation space type not supported"
+
+        assert (isinstance(env.action_space, spaces.Box) or
+                isinstance(env.action_space, spaces.Discrete)), "Action space type not supported"
+
+        actions = []
+        observations = []
+        rewards = []
+        episode_returns = np.zeros((n_episodes,))
+
+        episode_starts = []
+        gamma = model.gamma
+
+        ep_idx = 0
+        h_step = 0
+        obs = env.reset()
+        episode_obs = []
+        episode_obs.append([])
+        episode_gammas = []
+        episode_gammas.append([])
+        episode_act = []
+        episode_act.append([])
+        episode_starts.append(True)
+        reward_sum = 0.0
+        idx = 0
+        # state and mask for recurrent policies
+        state, mask = None, None
+
+        if is_vec_env:
+            mask = [True for _ in range(env.num_envs)]
+
+        while ep_idx < n_episodes:
+            obs_ = obs[0] if is_vec_env else obs
+            observations.append(obs_)
+            episode_obs[ep_idx].append(obs_)
+
+            if isinstance(model, BaseRLModel):
+                action, state = model.predict(obs, state=state, mask=mask)
+            else:
+                action = model(obs)
+
+            obs, reward, done, _ = env.step(action)
+
+            # Use only first env
+            if is_vec_env:
+                mask = [done[0] for _ in range(env.num_envs)]
+                action = np.array([action[0]])
+                reward = np.array([reward[0]])
+                done = np.array([done[0]])
+
+            actions.append(action)
+            episode_gammas[ep_idx].append(gamma ** h_step)
+            episode_act[ep_idx].append(action)
+            rewards.append(reward)
+            episode_starts.append(done)
+            reward_sum += reward
+            idx += 1
+            h_step += 1
+            if done:
+                if not is_vec_env:
+                    obs = env.reset()
+                    # Reset the state in case of a recurrent policy
+                    state = None
+                episode_returns[ep_idx] = reward_sum
+                reward_sum = 0.0
+                h_step = 0
+                ep_idx += 1
+                if ep_idx < n_episodes:
+                    episode_obs.append([])
+                    episode_act.append([])
+                    episode_gammas.append([])
+
+
+
+        if isinstance(env.observation_space, spaces.Box):
+            observations = np.concatenate(observations).reshape((-1,) + env.observation_space.shape)
+        elif isinstance(env.observation_space, spaces.Discrete):
+            observations = np.array(observations).reshape((-1, 1))
+
+        if isinstance(env.action_space, spaces.Box):
+            actions = np.concatenate(actions).reshape((-1,) + env.action_space.shape)
+        elif isinstance(env.action_space, spaces.Discrete):
+            actions = np.array(actions).reshape((-1, 1))
+
+
+
+        for ep_idx, (ep_obs, ep_act), in enumerate(zip(episode_obs, episode_act)):
+            for idx, (obs, act) in enumerate(zip(reversed(ep_obs), reversed(ep_act))):
+                current_features = np.concatenate((obs, act), axis=0)
+                if idx == 0:
+                    successor_features = (1-gamma) * current_features
+                else:
+                    successor_features = np.add(gamma * successor_features, (1 - gamma) * current_features)
+            if ep_idx == 0:
+                sum_successor_features = successor_features
+            else:
+                sum_successor_features = np.add(sum_successor_features, successor_features)
+
+        successor_features = sum_successor_features / n_episodes
+
+        rewards = np.array(rewards)
+        episode_starts = np.array(episode_starts[:-1])
+
+        assert len(observations) == len(actions)
+
+        return successor_features
+
+    def get_feature_expectation(self, model, env, n_episodes=10):
+        feat_exp = self.get_feature_expectation_raw(model, env.envs[0].env, n_episodes)
+        feat_exp = feat_exp * 100 # gamma has to be 0.99. successor_features has extra * (1 - gamma)
+        feat_exp = feat_exp[:-1] * self.normalize_s + self.normalize_b
+        return feat_exp
+
+    def linear_oracle(self, multiplicative_term, set_K):
+
+        # Max mode
+        max_dot_prod = -1e9
+        max_y = set_K[0]
+        idx = 0
+        for i, y_k in enumerate(set_K):
+            dot_prod = np.dot(multiplicative_term, y_k)
+            if dot_prod > max_dot_prod:
+                max_dot_prod = dot_prod
+                max_y = y_k
+                idx = i
+
+        return idx, max_y
+
+    def linesearch(self, x, gamma_max, d):
+        # h function is distance square between expert and the current expectations
+
+        # Let's use backtracking linesearch
+        c = 0.01
+        step_size = gamma_max
+        beta = 0.9
+        grad = x - self.expert_feat_exp
+        x_stepped = x + step_size * d
+        RHS = 0.5*LA.norm(x - self.expert_feat_exp, 2) + c*step_size*np.dot(grad, d)
+
+        while 0.5*LA.norm(x_stepped - self.expert_feat_exp, 2) > RHS:
+            step_size = beta * step_size
+            x_stepped = x + step_size * d
+            RHS = 0.5*LA.norm(x - self.expert_feat_exp, 2) + c*step_size*np.dot(grad, d)
+
+        return step_size
+
+    def learn(self, total_timesteps, num_proj_iters=5, callback=None, log_interval=2000, tb_log_name="Proj_MDPO_OFF",
+              reset_num_timesteps=True):
+        assert self.expert_dataset is not None, "You must pass an expert dataset to Proj_MDPO_OFF for training"
+
+        feat_exps_bar = []
+        feat_exps = []
+        ts = []
+        policy_weights = []
+        env = self.kwargs['env']
+
+        # Initialize
+        # kwargs = copy.deepcopy(self.kwargs)
+        # kwargs['tensorboard_log'] = os.path.join(kwargs['tensorboard_log'], 'iter%04d' % 0)
+        if self.mdpSolver == "PG":
+            model = PG(**self.kwargs)
+        else:
+            model = Proj_MDPO_OFF(**self.kwargs)
+        feat_exp = self.get_feature_expectation(model, env, 100)
+        model.save(os.path.join(self.logdir, 'iter%04d' % 0))
+        del model
+        feat_exps.append(feat_exp)
+        feat_exps_bar.append(feat_exp)
+        policy_weights.append(np.array([1.], dtype=np.float32))
+
+        # K set ~ simulate the K set
+        K = []
+        K.append(feat_exp)
+
+        # Vertices set
+        S = []
+        S.append(feat_exp)
+
+        # Alphas is a dictionary contains coefficients of all the
+        alphas = {}
+        # Turn this vector to a string
+        alphas[repr(feat_exp)] = 1
+
+        for i in range(1, num_proj_iters + 1):
+            new_S = []
+            print('proj iter:', i)
+
+            # MDPO can be replaced
+            model = Proj_MDPO_OFF(**self.kwargs)
+            model.reward_giver.update_reward(self.expert_feat_exp - feat_exps_bar[-1])
+            model.learn(total_timesteps // num_proj_iters, tb_log_name='iter%04d' % i)
+
+            print("Reward is ")
+            # Compute the New Policy's feature expectation
+            feat_exp = self.get_feature_expectation(model, env, 100)
+            model.save(os.path.join(self.logdir, 'iter%04d' % i))
+            del model
+            feat_exps.append(feat_exp)
+            K.append(feat_exp)
+
+            # h(phi) = (phi - self.expert_feat_exp)^2/2
+            # Line 4 of ASCG algo, implement a linear oracle here
+            h_deriv = (feat_exps_bar[-1] - self.expert_feat_exp)
+            # Then find y_t
+            y_idx, y_t = self.linear_oracle(-h_deriv, K)
+            d_FW = y_t - feat_exps_bar[-1]
+
+            # Line 5 of ASCG algo
+            z_idx, z_t = self.linear_oracle(h_deriv, S)
+            d_AS = feat_exps_bar[-1] - z_t
+            print("z_t is {}".format(z_t))
+
+            FW_step = False
+            # Just initializing them to 0
+            d = 0
+            gamma_max = 0
+            # Line 6 - 10
+            if np.dot(h_deriv, d_FW) < np.dot(h_deriv, d_AS):
+                FW_step = True
+                d = d_FW
+                gamma_max = 1
+            else:
+                FW_step = False
+                d = d_AS
+                if alphas[repr(z_t)] == 1:
+                    gamma_max = 100
+                else:
+                    gamma_max = alphas[repr(z_t)] / (1 - alphas[repr(z_t)])
+
+            # Line search
+            gamma_t = self.linesearch(feat_exps_bar[-1], gamma_max, d)
+            print()
+            # Update
+            feat_exp_bar = feat_exp + gamma_t * d
+            feat_exps_bar.append(feat_exp_bar)
+
+            # Update representation
+            if FW_step:
+                if gamma_t == 1:
+                    new_S = [y_t]
+                    alphas[repr(list(y_t))] = 1
+                else:
+                    print("y_t is {}".format(y_t))
+                    alphas[repr(list(y_t))] = (1 - gamma_t) * alphas[repr(list(y_t))] + gamma_t
+                    for idx, y in enumerate(S):
+                        alphas[repr(list(y))] = (1 - gamma_t) * alphas[repr(list(y))]
+                    S.append(y_t)
+                    new_S = S
+            # Away step
+            else:
+                # if gamma_t == gamma_max:
+                #     print("I'm here")
+                #     print(S)
+                #     S.pop(z_idx)
+                #     new_S = S
+                # A little hack here, to avoid an empty S set
+                alphas[repr(list(z_t))] = (1 - gamma_t) * alphas[repr(list(z_t))] - gamma_t
+                for idx, y in enumerate(S):
+                    alphas[repr(list(y))] = (1 + gamma_t) * alphas[repr(list(y))]
+                new_S = S
+
+            # Assign previous
+            S = new_S
+
+            # Update the mixture policy
+            policy_weight_pre = np.concatenate([policy_weights[-1], np.array([0.], dtype=np.float32)], axis=0)
+            policy_weight_i = np.zeros((i + 1,), dtype=np.float32)
+            policy_weight_i[-1] = 1
+            assert policy_weight_pre.shape == policy_weight_i.shape
+            policy_weight = policy_weight_pre + gamma_t * (policy_weight_i - policy_weight_pre)
+            policy_weights.append(policy_weight)
+
+        with open(os.path.join(self.logdir, 'data.pkl'), 'wb') as f:
+            pickle.dump({'feat_exps_bar': feat_exps_bar, 'policy_weights': policy_weights, 'feat_exps': feat_exps}, f)
+
+    def save(self, save_path):
+        pass
+
 
 
 from stable_baselines.sac import SAC
